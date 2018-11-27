be0ac9d8b93a598028f80c5ac2bca6c1d758fe32
holysabre <1026579245@qq.com>
2018-11-27 17:49:42 +0800

    To solve Bug

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,17 +10,10 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
 
-<<<<<<< HEAD
-    $router->get('/', 'HomeController@articles');
-    $router->resource('categories', CategoryController::class);
-    $router->resource('modules', ModuleController::class);
-    $router->resource('pages', PageController::class);
-    $router->resource('articles111', ArticleController::class);
-=======
     $router->get('/', 'HomeController@index');
     $router->resource('pages', PageController::class);
     $router->resource('categories', CategoryController::class);
     $router->resource('modules', ModuleController::class);
->>>>>>> parent of aa82eca... Add admin--article,update job--TranslateSlug,make it can translate title from different table
+    $router->resource('articles', ArticleController::class);
 
 });
